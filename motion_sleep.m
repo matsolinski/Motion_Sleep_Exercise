fecf7999fa69de2acdf9e7aa0b1db1572e54008e
matsolinski <mat.solinski@gmail.com>
2015-07-05 02:58:41 +0200

    v2_motion_vector
    1. Load files
    2. Plot motion from 3 axis and sleep phases
    3. Calculate motion vector

--- motion_sleep.m
+++ motion_sleep.m
@@ -28,6 +28,34 @@ timestampSP = out{1};
 sleepP = out{2};
 
 
+%----- create motion vector
+
+
+absInstMot = sqrt(Xacc(1:end).^2 + Yacc(1:end).^2 + Zacc(1:end).^2 );
+motionVect =  abs(diff(absInstMot));
+figure(3)
+  plot(motionVect)
+
+  %alternative version
+%absInstMot = sqrt( (Xacc(2:end)-Xacc(1:end-1)).^2 + (Yacc(2:end)-Yacc(1:end-1)).^2 + (Zacc(2:end)-Zacc(1:end-1)).^2 );
+%motionVect =  absInstMot;
+%figure(2)
+%  plot(motionVect)
+
 %----- print
-figure(1)
-plot(sleepP)
+show=0;
+if(show)
+  figure(1)
+    subplot(4,1,1)
+      plot(Xacc);
+      title('Accelerometer X-axis');
+    subplot(4,1,2)
+      plot(Yacc)
+      title('Accelerometer Y-axis');
+    subplot(4,1,3)
+      plot(Zacc)
+      title('Accelerometer Z-axis');
+    subplot(4,1,4)
+      plot(sleepP)
+      title('Sleep Phases');  
+end
